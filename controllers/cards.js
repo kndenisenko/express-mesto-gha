fbdd485cf761affa6c86715d3287cee8039bc6a3
Konstantin Denisenko <denisenko.constantin2016@yandex.ru>
2022-12-06 18:43:23 +0300

    фиксы тестов практикума

--- controllers/cards.js
+++ controllers/cards.js
@@ -26,7 +26,6 @@ module.exports.createCard = (req, res) => {
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send({ data: card }))
     .catch((err) => {
-      console.log(err);
       if (err.name === 'ValidationError') {
         return res.status(400).send({
           message: '400 — Переданы некорректные данные при создании карточки.',
