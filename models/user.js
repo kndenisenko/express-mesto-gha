9c817293b1ff780ad99fd5654bcef9688e5757f0
Konstantin Denisenko <denisenko.constantin2016@yandex.ru>
2022-12-12 18:09:48 +0300

    всё сломалось

--- models/user.js
+++ models/user.js
@@ -1,41 +1,21 @@
 const mongoose = require('mongoose');
-// const bcrypt = require('bcrypt');
-const isEmail = require('validator/lib/isEmail');
-
-// const saltRounds = 10;
-// const myPlaintextPassword = 's0/\/\P4$$w0rD';
-// const someOtherPlaintextPassword = 'not_bacon';
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String, // тип поля - строка
-    default: 'Жак-Ив Кусто',
+    required: true, // обязательное поле
     minlength: 2, // минимальное количество символов
     maxlength: 30, // максимальное количество символов
   },
-  email: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (v) => isEmail(v),
-      message: 'Неправильный формат адреса электронной почты',
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-    select: false,
-  },
   about: {
     type: String,
-    default: 'Исследователь',
+    required: true,
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
     type: String,
-    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    required: true,
   },
 });
 
