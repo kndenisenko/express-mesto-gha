f3975bfb8b374b4ba0e115d8a6fff0e7be4cb6bf
Konstantin Denisenko <denisenko.constantin2016@yandex.ru>
2022-12-12 18:06:58 +0300

    автотесты 13 спринт

--- models/user.js
+++ models/user.js
@@ -1,21 +1,41 @@
 const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
+const isEmail = require('validator/lib/isEmail');
+
+// const saltRounds = 10;
+// const myPlaintextPassword = 's0/\/\P4$$w0rD';
+// const someOtherPlaintextPassword = 'not_bacon';
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String, // тип поля - строка
-    required: true, // обязательное поле
+    default: 'Жак-Ив Кусто',
     minlength: 2, // минимальное количество символов
     maxlength: 30, // максимальное количество символов
   },
-  about: {
+  email: {
     type: String,
     required: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Неправильный формат адреса электронной почты',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false,
+  },
+  about: {
+    type: String,
+    default: 'Исследователь',
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
     type: String,
-    required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
 });
 
